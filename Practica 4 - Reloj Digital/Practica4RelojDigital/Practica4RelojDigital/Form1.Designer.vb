5a2d387386ca504b94904c61b4afcee5de5ee200
alejandro-segura1 <alejandro.segura@iest.edu.mx>
2026-03-09 08:37:02 -0600

    Reloj Digital Version 1.0
    Se añadieron las Horas, Minutos, Segundos y la Fecha

---
+++ Practica 4 - Reloj Digital/Practica4RelojDigital/Practica4RelojDigital/Form1.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Horas = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Minutos = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Segundos = New System.Windows.Forms.Label()
+        Me.Dias = New System.Windows.Forms.Label()
+        Me.Fecha = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Horas
+        '
+        Me.Horas.AutoSize = True
+        Me.Horas.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Horas.Location = New System.Drawing.Point(52, 63)
+        Me.Horas.Name = "Horas"
+        Me.Horas.Size = New System.Drawing.Size(215, 153)
+        Me.Horas.TabIndex = 0
+        Me.Horas.Text = "00"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(226, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 153)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = ":"
+        '
+        'Minutos
+        '
+        Me.Minutos.AutoSize = True
+        Me.Minutos.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Minutos.Location = New System.Drawing.Point(284, 63)
+        Me.Minutos.Name = "Minutos"
+        Me.Minutos.Size = New System.Drawing.Size(215, 153)
+        Me.Minutos.TabIndex = 2
+        Me.Minutos.Text = "00"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(459, 63)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 153)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = ":"
+        '
+        'Segundos
+        '
+        Me.Segundos.AutoSize = True
+        Me.Segundos.Font = New System.Drawing.Font("Microsoft Sans Serif", 100.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Segundos.Location = New System.Drawing.Point(519, 63)
+        Me.Segundos.Name = "Segundos"
+        Me.Segundos.Size = New System.Drawing.Size(215, 153)
+        Me.Segundos.TabIndex = 4
+        Me.Segundos.Text = "00"
+        '
+        'Dias
+        '
+        Me.Dias.AutoSize = True
+        Me.Dias.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Dias.Location = New System.Drawing.Point(122, 206)
+        Me.Dias.Name = "Dias"
+        Me.Dias.Size = New System.Drawing.Size(133, 46)
+        Me.Dias.TabIndex = 5
+        Me.Dias.Text = "Lunes"
+        '
+        'Fecha
+        '
+        Me.Fecha.AutoSize = True
+        Me.Fecha.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Fecha.Location = New System.Drawing.Point(244, 206)
+        Me.Fecha.Name = "Fecha"
+        Me.Fecha.Size = New System.Drawing.Size(424, 46)
+        Me.Fecha.TabIndex = 6
+        Me.Fecha.Text = "09 de Marzo del 2026"
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 335)
+        Me.Controls.Add(Me.Fecha)
+        Me.Controls.Add(Me.Dias)
+        Me.Controls.Add(Me.Segundos)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Minutos)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Horas)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Reloj Digital"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Horas As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Minutos As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Segundos As Label
+    Friend WithEvents Dias As Label
+    Friend WithEvents Fecha As Label
+    Friend WithEvents Timer1 As Timer
+End Class
